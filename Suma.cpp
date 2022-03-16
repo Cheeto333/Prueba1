4f3ed195940b5a884985f2f41328850eb1220ca8
Cheeto333 <101517128+Cheeto333@users.noreply.github.com>
2022-03-16 08:27:01 -0500

    Add files via upload

--- Suma.cpp
+++ Suma.cpp
@@ -3,8 +3,11 @@ using namespace std;
 int main()
 {
 	float a, b, c;
+	
+	cout<<"Ingrese 2 numeros" << "para realizar la suma"<<;
 	cin>>a>>b;
 	c=a+b;
+	cout<<"El resultado es:"
 	cout<<c;
 	return 0;
 }
