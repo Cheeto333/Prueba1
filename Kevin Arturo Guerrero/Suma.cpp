4c487bf9ef2074031b4c6dc416faf750fca7a370
Cheeto333 <101517128+Cheeto333@users.noreply.github.com>
2022-03-16 08:57:36 -0500

    Update Suma.cpp

--- Kevin Arturo Guerrero/Suma.cpp
+++ Kevin Arturo Guerrero/Suma.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main()
 {
 	float a, b, c;
+	cout<<"Ingrese 2 numeros para realizar la suma"<<endl;
 	cin>>a>>b;
 	c=a+b;
+	cout<<"El resultado es:"<<endl;
 	cout<<c;
 	return 0;
 }
